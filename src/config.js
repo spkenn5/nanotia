cdff8394b9425a97fd3e25e1f15c609e4b205322
kenji-v <kenji-v@crimsonlogic.com>
2018-11-27 19:54:11 +0800

    init

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const defaultConfig = {
+    mainPage: 'https://www.techinasia.com/wp-json/techinasia/2.0/posts',
+    postPage: `https://www.techinasia.com/wp-json/techinasia/2.0/posts/${articleLink}`,
+    related: `https://www.techinasia.com/wp-json/techinasia/2.0/posts/${articleId}/related`
+}
---
+++ src/page/HomePage.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                <div className="jumbotron text-center">
+                    <h1>My First Bootstrap Page</h1>
+                    <p>Resize this responsive page to see the effect!</p>
+                </div>
+                <div className = "container" >
+
+                </div>
+            </div>
+        );
+    }
+}
